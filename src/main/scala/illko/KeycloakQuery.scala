375b2c4128ec992692136d4e37c4f8e635abe065
IKorniiko <IKorniiko@luxoft.com>
2018-12-31 16:32:47 +0200

    creating/searching/deleting user

--- src/main/scala/illko/KeycloakQuery.scala
+++ src/main/scala/illko/KeycloakQuery.scala
@@ -1,7 +1,9 @@
 package illko
 
 import org.keycloak.admin.client.Keycloak
-import org.keycloak.admin.client.resource.ClientsResource
+import org.keycloak.admin.client.resource.{ClientsResource, RealmResource, UsersResource}
+import org.keycloak.representations.idm.UserRepresentation
+
 import scala.collection.JavaConverters._
 
 object KeycloakQuery {
@@ -11,10 +13,50 @@ object KeycloakQuery {
 
     val keycloak: Keycloak = Keycloak.getInstance(authServer, "example", "examples-admin-client", "password",
       "examples-admin-client", "password")
-    val clients: ClientsResource = keycloak.realm("example").clients()
+    val realm = keycloak.realm("example")
 
+    printClients(realm)
+
+    val usersResource = realm.users()
+    userOperations(usersResource)
+  }
+
+  private def userOperations(usersResource: UsersResource) = {
+    printUsers(usersResource.list().asScala)
+    createUser(usersResource)
+
+    printUsers(usersResource.list().asScala)
+
+    val found = usersResource.search("testUser1").asScala
+    printUsers(found)
+
+    found.headOption.foreach { user =>
+      usersResource.delete(user.getId)
+    }
+
+    printUsers(usersResource.list().asScala)
+  }
+
+  private def createUser(usersResource: UsersResource) = {
+    val user = new UserRepresentation()
+    user.setUsername("testUser1")
+    user.setFirstName("First Name")
+    user.setLastName("Last Name")
+    user.setEmail("1@b.com")
+    user.setEnabled(true)
+    val response = usersResource.create(user)
+    println("status: " + response.getStatusInfo.toEnum)
+  }
+
+  private def printUsers(users: Seq[UserRepresentation]) = {
+    for ((user, i) <- users.zipWithIndex) {
+      println(s"${i + 1}. id: ${user.getId}, username: ${user.getUsername}, Name: ${user.getFirstName} ${user.getLastName}, email: ${user.getEmail}, enabled: ${user.isEnabled}")
+    }
+  }
+
+  def printClients(realm: RealmResource) = {
+    val clients = realm.clients()
     for (client <- clients.findAll().asScala) {
-
       if (client.getBaseUrl != null) {
         println("client url: " + client.getBaseUrl + ", client id: " + client.getClientId)
       } else {
