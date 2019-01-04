cc05615681b0ffd11a0519bdbb9e3077931f005a
ilya <intracer@gmail.com>
2019-01-04 09:24:32 +0200

    more tests

--- src/test/scala/illko/KeycloakSpec.scala
+++ src/test/scala/illko/KeycloakSpec.scala
@@ -1,7 +1,10 @@
 package illko
 
+import javax.ws.rs.core.Response.Status
 import org.keycloak.admin.client.Keycloak
+import org.keycloak.representations.idm.UserRepresentation
 import org.specs2.mutable.Specification
+
 import scala.collection.JavaConverters._
 
 class KeycloakSpec extends Specification {
@@ -20,6 +23,40 @@ class KeycloakSpec extends Specification {
       val client = clientMap("examples-admin-client")
       client.getBaseUrl === "/examples-admin-client"
       client.getClientId === "examples-admin-client"
+    }
+
+    "contain example user" in {
+      val usersResource = realm.users()
+      val users = usersResource.list().asScala
+      users.size === 1
+      val user = users.head
+      user.getUsername === "examples-admin-client"
+      user.isEnabled === true
+    }
+
+    "create and delete user" in {
+      val usersResource = realm.users()
+      usersResource.list().asScala.size === 1
+
+      val user = new UserRepresentation()
+      user.setUsername("testUser1")
+      user.setFirstName("First Name")
+      user.setLastName("Last Name")
+      user.setEmail("1@b.com")
+      user.setEnabled(true)
+      val createResponse = usersResource.create(user)
+      createResponse.getStatusInfo.toEnum === Status.CREATED
+
+      val all = usersResource.list().asScala
+      all.size === 2
+
+      val found = usersResource.search("testUser1").asScala
+      found.size === 1
+
+      val deleteResponse = usersResource.delete(found.head.getId)
+      deleteResponse.getStatusInfo.toEnum === Status.NO_CONTENT
+
+      usersResource.search("testUser1").asScala.isEmpty === true
     }
   }
 }
